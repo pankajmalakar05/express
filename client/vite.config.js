b1b3bab5f37b7f8c801fc8c8f5953bdc21f30c37
pankaj malakar <mrpankajmalakar@gmail.com>
2026-02-06 18:02:32 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import.meta.env.VITE_API_URL
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/ansexpress-website' : '/express', 
-})
+});
+
